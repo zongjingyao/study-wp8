cf95000a32934cae267804ce1f32336972f2938d
zongzongzi <zongjingyao@gmail.com>
2014-01-15 21:21:37 +0800

    添加RadioButton单选按钮

--- examples/MainPage.xaml.cs
+++ examples/MainPage.xaml.cs
@@ -47,6 +47,11 @@ namespace examples
             NavigationService.Navigate(new Uri("/HyperlinkButtonPage.xaml", UriKind.Relative));
         }
 
+        private void btnToRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/RadioButtonPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
---
+++ examples/RadioButtonPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace examples
+{
+    public partial class RadioButtonPage : PhoneApplicationPage
+    {
+        public RadioButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (a.IsChecked == true)
+                answer.Text = a.Content.ToString();
+            else if (b.IsChecked == true)
+                answer.Text = b.Content.ToString();
+            else if (c.IsChecked == true)
+                answer.Text = c.Content.ToString();
+            else
+                answer.Text = d.Content.ToString();
+        }
+    }
+}
