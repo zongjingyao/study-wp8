e9956bbb8555969ba0e6ee8a7dd306f43334f07c
zongzongzi <zongjingyao@gmail.com>
2014-01-15 16:24:25 +0800

    增加TextBox

--- examples/MainPage.xaml.cs
+++ examples/MainPage.xaml.cs
@@ -32,6 +32,11 @@ namespace examples
             NavigationService.Navigate(new Uri("/TextBlockPage.xaml", UriKind.Relative));
         }
 
+        private void btnToTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/TextBoxPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
---
+++ examples/TextBoxPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace examples
+{
+    public partial class TextBoxPage : PhoneApplicationPage
+    {
+        public TextBoxPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBlock1.Text = textBox1.Text;
+        }
+    }
+}
