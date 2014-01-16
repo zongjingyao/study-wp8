704c67e465a7a9756388af8f4e3076ae9085621f
zongzongzi <zongjingyao@gmail.com>
2014-01-16 19:39:11 +0800

    增加ScrollViewer滚动区域控件

--- examples/MainPage.xaml.cs
+++ examples/MainPage.xaml.cs
@@ -62,6 +62,11 @@ namespace examples
             NavigationService.Navigate(new Uri("/ProgressBarPage.xaml", UriKind.Relative));
         }
 
+        private void btnToScrollViewer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ScrollViewerPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
