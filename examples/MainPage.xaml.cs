5f35c4d4c493845abba81fe4283340093efde81a
zongzongzi <zongjingyao@gmail.com>
2014-01-17 16:31:57 +0800

    添加ListBox列表框控件

--- examples/MainPage.xaml.cs
+++ examples/MainPage.xaml.cs
@@ -67,6 +67,11 @@ namespace examples
             NavigationService.Navigate(new Uri("/ScrollViewerPage.xaml", UriKind.Relative));
         }
 
+        private void btnToListBox_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ListBoxPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
