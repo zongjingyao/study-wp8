070baa5beb31eae146d03af9d763503fdd053ad9
zongzongzi <zongjingyao@gmail.com>
2014-01-17 17:52:35 +0800

    添加ApplicationBar菜单栏

--- examples/MainPage.xaml.cs
+++ examples/MainPage.xaml.cs
@@ -77,6 +77,11 @@ namespace examples
             NavigationService.Navigate(new Uri("/SliderPage.xaml", UriKind.Relative));
         }
 
+        private void btnToApplicationBar_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ApplicationBarPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
