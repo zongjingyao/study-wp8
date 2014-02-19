563b05bf9c6031eb468473de55cf6b74e079a28a
zongjingyao <zongjingyao@gmail.com>
2014-02-19 16:28:55 +0800

    增加连接设置(ConnectionSettingTask)

---
+++ LaunchersAndChoosersDemo/ConnectionSettingTaskPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace LaunchersAndChoosersDemo
+{
+    public partial class ConnectionSettingTaskPage : PhoneApplicationPage
+    {
+        ConnectionSettingsTask connectionSettingsTask;
+        public ConnectionSettingTaskPage()
+        {
+            InitializeComponent();
+            //创建一个连接设置任务
+            connectionSettingsTask = new ConnectionSettingsTask();
+        }
+
+        private void btnWiFi_Click(object sender, RoutedEventArgs e)
+        {
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.WiFi;
+            connectionSettingsTask.Show();
+        }
+
+        private void btnBluetooth_Click(object sender, RoutedEventArgs e)
+        {
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.Bluetooth;
+            connectionSettingsTask.Show();
+        }
+
+        private void btnCellular_Click(object sender, RoutedEventArgs e)
+        {
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.Cellular;
+            connectionSettingsTask.Show();
+        }
+
+        private void btnAirplaneMode_Click(object sender, RoutedEventArgs e)
+        {
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.AirplaneMode;
+            connectionSettingsTask.Show();
+        }
+    }
+}
--- LaunchersAndChoosersDemo/MainPage.xaml.cs
+++ LaunchersAndChoosersDemo/MainPage.xaml.cs
@@ -77,6 +77,11 @@ namespace LaunchersAndChoosersDemo
             NavigationService.Navigate(new Uri("/MapsTaskPage.xaml", UriKind.Relative));
         }
 
+        private void btnConnectionSettingTask_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ConnectionSettingTaskPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
