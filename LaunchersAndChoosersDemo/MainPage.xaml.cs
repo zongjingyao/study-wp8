56ee64638675658ab775d12397a9c05713723d47
zongjingyao <zongjingyao@gmail.com>
2014-02-19 13:16:27 +0800

    增加启动浏览器(WebBrowserTask)

--- LaunchersAndChoosersDemo/MainPage.xaml.cs
+++ LaunchersAndChoosersDemo/MainPage.xaml.cs
@@ -42,6 +42,11 @@ namespace LaunchersAndChoosersDemo
             NavigationService.Navigate(new Uri("/SmsComposeTaskPage.xaml", UriKind.Relative));
         }
 
+        private void btnWebBrowserTask_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/WebBrowserTaskPage.xaml", UriKind.Relative));
+        }
+
         // 用于生成本地化 ApplicationBar 的示例代码
         //private void BuildLocalizedApplicationBar()
         //{
---
+++ LaunchersAndChoosersDemo/WebBrowserTaskPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace LaunchersAndChoosersDemo
+{
+    public partial class WebBrowserTaskPage : PhoneApplicationPage
+    {
+        public WebBrowserTaskPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask wbt = new WebBrowserTask();
+            wbt.URL = tbxUrl.Text;
+            wbt.Show();
+        }
+    }
+}
