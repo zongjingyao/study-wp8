feeea9490ee352fd8a7e044ba02449257889ef47
zongjingyao <zongjingyao@gmail.com>
2014-03-09 09:43:17 +0800

    Done!

---
+++ BindingDataDemo/Model/Food.cs
@@ -0,0 +1,30 @@
+﻿
+namespace BindingDataDemo.Model
+{
+    public class Food
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public string IconUri
+        {
+            get;
+            set;
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+    }
+}
